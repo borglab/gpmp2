7532bc8f6131a976dd48eabed5e0626690c0b452
Varun Agrawal <varagrawal@gmail.com>
2023-04-07 17:29:40 -0400

    initial python testing framework

---
+++ python/tests/test_gpmp2.py
@@ -0,0 +1,13 @@
+# pylint: disable=no-name-in-module, import-error, no-member
+import os.path as osp
+import unittest
+
+import numpy as np
+from gtsam import Point3, Pose3, Rot3
+from gtsam.utils.test_case import GtsamTestCase
+
+import gpmp2
+
+# class TestClass(unittest.TestCase):
+#     def test_main(self):
+#         print("Testing 123")
