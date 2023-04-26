3796985f38e6817d4e3c7f23e228f81453bd655c
Varun Agrawal <varagrawal@gmail.com>
2023-04-26 11:56:24 -0400

    add geometry and utils tests

--- python/tests/test_geometry.py
+++ python/tests/test_geometry.py
@@ -1,4 +1,4 @@
-"""Tests for the `geometry module`."""
+"""Tests for the `geometry` module."""
 
 import gtsam
 import numpy as np
@@ -15,5 +15,19 @@ class TestPose2Vector(GtsamTestCase):
         pose = gtsam.Pose2(0, 0, 0)
         c = np.random.rand(3)
         pose2vector = gpmp2.Pose2Vector(pose, c)
+        self.assertIsNotNone(pose2vector)
+
+    def test_pose(self):
+        """Test the Pose2Vector pose method."""
+        pose = gtsam.Pose2(0, 0, 0)
+        c = np.random.rand(3)
+        pose2vector = gpmp2.Pose2Vector(pose, c)
         self.assertTrue(pose2vector.pose().equals(pose, 1e-9))
+        np.testing.assert_equal(pose2vector.configuration(), c)
+
+    def test_configuration(self):
+        """Test the Pose2Vector configuration method."""
+        pose = gtsam.Pose2(0, 0, 0)
+        c = np.random.rand(3)
+        pose2vector = gpmp2.Pose2Vector(pose, c)
         np.testing.assert_equal(pose2vector.configuration(), c)
